37b6e96ba091b51d9e92e9ff9e22b61b80125d0c
Emilien Andre <emilien.andre2764@gmail.com>
2023-04-14 22:20:37 +0200

    first commit

---
+++ publish.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+GMAD=~/.local/share/Steam/steamapps/common/GarrysMod/bin/gmad_linux
+GMPUBLISH=~/.local/share/Steam/steamapps/common/GarrysMod/bin/gmpublish_linux
+LD_LIBRARY_PATH=~/.local/share/Steam/steamapps/common/GarrysMod/bin/
+export LD_LIBRARY_PATH
+$GMAD create -folder . -out ../city_bus.gma
+$GMPUBLISH create -addon ../city_bus.gma -icon icon.jpg
+rm ../city_bus.gma
